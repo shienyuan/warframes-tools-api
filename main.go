1701b348aab2f11fa78f54d981ccc229d6ad1aaa
sy97 <shienyuan97@gmail.com>
2020-12-31 15:17:09 +1300

    INIT

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"warframes-tools-api/controller"
@@ -10,15 +9,6 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("--------------------------")
-	log.Println(os.Getenv("DB"))
-	log.Println(os.Getenv("PORT"))
-	log.Println("--------------------------")
-
 	model.SetupDb()
 
 	r := gin.Default()
@@ -27,6 +17,5 @@ func main() {
 
 	warframe.GET("", controller.GetWarframes)
 
-	log.Fatal(r.Run(":" + os.Getenv("PORT"))) // listen and serve on 0.0.0.0:8080 (
-	// for windows "localhost:8080")
+	log.Fatal(r.Run(":" + os.Getenv("PORT")))
 }
